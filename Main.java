2ce63e76bc4e814de8f15ee8e1bdd4f64175e942
Kyle Burden <74677382+KyleBurden@users.noreply.github.com>
2022-07-31 14:43:59 -0230

    Add files via upload
    Started main class and word bank class.

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Hangman;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to our Hangman Game!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Press 1 for easy, 2 for medium, 3 for hard.");
+    }
+}
---
+++ Words.java
@@ -0,0 +1,57 @@
+package Hangman;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.*;
+
+public class Words {
+    ArrayList<String> easyWords = new ArrayList<String>();
+
+    public static void main(String[] args){
+
+
+
+    }
+    public static ArrayList<String> chooseDiff(String diffChoice) throws FileNotFoundException {
+        File theFile = new File("Easy_Words");
+        Scanner file = new Scanner(theFile);
+        ArrayList<String> easyWords = new ArrayList<String>();
+        while (file.hasNext()) {
+            easyWords.add(file.nextLine());
+        }
+        File theFile2 = new File("Medium_Words");
+        Scanner file2 = new Scanner(theFile2);
+        ArrayList<String> mediumWords = new ArrayList<String>();
+        while (file2.hasNext()) {
+            mediumWords.add(file2.nextLine());
+        }
+        File theFile3 = new File("Hard_Words");
+        Scanner file3 = new Scanner(theFile3);
+        ArrayList<String> hardWords = new ArrayList<String>();
+        while (file3.hasNext()) {
+            mediumWords.add(file3.nextLine());
+        }
+
+
+        switch (diffChoice) {
+            case "1":
+                return easyWords;
+            case "2":
+                //retrieve medium words
+                return mediumWords;
+            case "3":
+                //retrieve hard words
+                return hardWords;
+        }
+        return easyWords;
+    }
+
+    public static String getTheWord(ArrayList<String> theWords){
+        Random ran = new Random();
+        int ranWord = ran.nextInt(theWords.size());
+        String wordRand = theWords.get(ranWord);
+        return wordRand;
+    }
+}
