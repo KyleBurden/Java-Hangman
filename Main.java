cedec300733c97337199cab79a639ffe1111a60c
Kyle Burden <74677382+KyleBurden@users.noreply.github.com>
2022-07-31 20:01:03 -0230

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 package Hangman;
 
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import static Hangman.Words.chooseDiff;
+import static Hangman.Words.getTheWord;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +14,9 @@ public class Main {
         System.out.println("Welcome to our Hangman Game!");
         Scanner scan = new Scanner(System.in);
         System.out.println("Press 1 for easy, 2 for medium, 3 for hard.");
+        String userChoice = scan.nextLine();
+        ArrayList<String> words = chooseDiff(userChoice);
+        String wordToGuess = getTheWord(words);
+        System.out.println(wordToGuess);
     }
 }
