2a37f56f3ded5aabc6eb126725a79f6ef5e63717
Kyle Burden <74677382+KyleBurden@users.noreply.github.com>
2022-08-08 20:51:24 -0230

    Update soundEffects.java

--- soundEffects.java
+++ soundEffects.java
@@ -1,3 +1,5 @@
+package Hangman
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,29 +10,19 @@ public class soundEffects {
 
 
     public static void correctSoundEffect() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        Scanner scan = new Scanner(System.in);
         File file = new File("applause_y.wav");
         AudioInputStream InputStream = AudioSystem.getAudioInputStream(file);
         Clip clip  = AudioSystem.getClip();
         clip.open(InputStream);
         clip.start();
-        String response = scan.next();
-
-
-
     }
 
     public static void incorrectSoundEffect() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
-
-        Scanner scan = new Scanner(System.in);
         File file = new File("quick_fart_x.wav");
         AudioInputStream InputStream = AudioSystem.getAudioInputStream(file);
         Clip clip  = AudioSystem.getClip();
         clip.open(InputStream);
-        clip.start();
-        String response = scan.next();
-
-
+        clip.start();   
 
     }
 
