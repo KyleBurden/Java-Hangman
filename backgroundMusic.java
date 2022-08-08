9db5fe80da50a54013e6c5f3a96691970e0e1ea2
Barryadam1103 <82234359+Barryadam1103@users.noreply.github.com>
2022-08-08 12:35:58 -0230

    Add files via upload
    Background sound and sound effects for game.

--- backgroundMusic.java
+++ backgroundMusic.java
@@ -1,28 +1,39 @@
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
-import java.nio.file.Paths;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class backgroundMusic {
-    MediaPlayer mediaPlayer;
-    public void playBackgroundMusic() {
-        String filename = "ice_cream_truck.wav";
-        Media p = new Media(Paths.get(filename).toUri().toString());
-        mediaPlayer = new MediaPlayer(p);
-        mediaPlayer.play();
+
+    public static void main(String args[]) {
+
+        try{
+            File file = new File("ice_cream_truck.wav");
+
+            if(file.exists()){
+                Scanner scan = new Scanner(System.in);
+                AudioInputStream AudioInput=  AudioSystem.getAudioInputStream(file);
+                Clip sound_clip = AudioSystem.getClip();
+                sound_clip.open(AudioInput);
+                sound_clip.start();
+                sound_clip.loop(Clip.LOOP_CONTINUOUSLY);
+                String response = scan.next();
+
+
+            }
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+
+
 
 
     }
-
-
-
-
-
-
-
-
-
-
 
 
 }
