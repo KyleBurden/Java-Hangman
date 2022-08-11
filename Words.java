b7ae2303ac098a4bda9b4a16a790ac4f79157027
Kyle Burden <74677382+KyleBurden@users.noreply.github.com>
2022-08-11 16:08:04 -0230

    Update Words.java

--- Words.java
+++ Words.java
@@ -1,18 +1,23 @@
 package Hangman;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.util.*;
 import java.io.*;
+
+import static Hangman.DrawMan.displayHangman;
 
 public class Words {
     private static int wrongGuesses = 0;
+    
+     //getter to see how many wrong guesses the user has made
 
     public static int getWrongGuesses() {
         return wrongGuesses;
     }
     
+    //This the the method to choose your diificulty: 1 for easy 2 for medium: 3 for hard:
+    //This will return an ArrayList of strings from the text file that the user chose
     public static ArrayList<String> chooseDiff(String diffChoice) throws FileNotFoundException {
         File theFile = new File("Easy_Words.txt");
         Scanner file = new Scanner(theFile);
@@ -46,22 +51,40 @@ public class Words {
         }
         return easyWords;
     }
-
+    
+//This gets a random word from the arraylist you pass into it
     public static String getTheWord(ArrayList<String> theWords){
         Random ran = new Random();
         int ranWord = ran.nextInt(theWords.size());
         String wordRand = theWords.get(ranWord);
         return wordRand;
     }
-     public static String theWord() throws FileNotFoundException {
+    
+     // This method takes input from the user. Calls chooseDiff and then calls getTheWord on the arraylist returned from chooseDiff
+    //returns the word that the user will have to guess
+    public static String theWord() throws FileNotFoundException {
         System.out.println("Press 1 for easy, 2 for medium, 3 for hard.");
         Scanner scan = new Scanner(System.in);
-        int userChoice = scan.nextInt();
-        ArrayList<String> words = chooseDiff(userChoice);
-        String wordToGuess = getTheWord(words);
-        return wordToGuess;
+        try{
+           int userChoice = scan.nextInt();
+            ArrayList<String> words = chooseDiff(userChoice);
+            String wordToGuess = getTheWord(words);
+            return wordToGuess;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (InputMismatchException ex){
+            System.err.println("Incorrect value entered");
+            ex.printStackTrace();
+        }
+
+
+        return null;
     }
     
+    //This method takes a list of characters and the string the user has to guess.
+    //Returns the wordState string
+    //if the list of characters contains the characters of the word add the letter... else: add an underscore
     public static String wordState(List<Character> allRightGuesses, String theWord){
         String wordsState ="";
         for (char letters :theWord.toCharArray()) {
@@ -75,7 +98,9 @@ public class Words {
            return wordsState;
     }
     
-    
+   //This returns nothing. Will check to see if letter is in the word and if its not it increases the wrong guesses int and displays the hangman art,
+    //and lets you know how many guesses you have left and prints your wrong guesses. If you guess the letter it will play the clapping sound.
+    //if you win it will print that you have won
     public static void hang (String guess, String wordToGuess, String underscore, ArrayList < String > allWrongGuesses, ArrayList < String > allRightGuesses) throws
                 IOException, UnsupportedAudioFileException, LineUnavailableException {
             String newunderscore = "";
@@ -91,6 +116,7 @@ public class Words {
 
             }
 
+        //If the letter is not in the word
             if (underscore.equals(newunderscore)) {
                 wrongGuesses++;
                 displayHangman(wrongGuesses);
@@ -101,10 +127,12 @@ public class Words {
                     System.out.print(theWrongGuess + " ");
 
                 }
+                //this runs if the letter is in the word
             } else {
                 soundEffects.correctSoundEffect();
                 underscore = newunderscore;
             }
+        //if you solved the word
             if (underscore.equals(wordToGuess)) {
                 System.out.println("You have solved the word. It was " + wordToGuess);
             }
