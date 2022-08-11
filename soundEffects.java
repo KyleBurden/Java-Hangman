2c7c5d933a975297bb676d8da22e453dd3e5170d
Kyle Burden <74677382+KyleBurden@users.noreply.github.com>
2022-08-11 16:00:45 -0230

    Update soundEffects.java

--- soundEffects.java
+++ soundEffects.java
@@ -2,12 +2,12 @@ package Hangman
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 import javax.sound.sampled.*;
 
 
 public class soundEffects {
 
+    //This is going to play the correct sound when a user guesses a right letter
 
     public static void correctSoundEffect() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         File file = new File("applause_y.wav");
@@ -16,7 +16,8 @@ public class soundEffects {
         clip.open(InputStream);
         clip.start();
     }
-
+    
+    //This is going to play the incorrect sound when a user guesses a wrong letter
     public static void incorrectSoundEffect() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
         File file = new File("quick_fart_x.wav");
         AudioInputStream InputStream = AudioSystem.getAudioInputStream(file);
